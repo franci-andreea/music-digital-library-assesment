aaffb471f7dc60d215aa5c02417cdaf9c3cd49a9
Franci <francesca1999@yahoo.com>
2024-07-15 13:34:21 +0300

    add path to view album page

--- src/components/artist_albums_list/ArtistAlbumsList.jsx
+++ src/components/artist_albums_list/ArtistAlbumsList.jsx
@@ -1,9 +1,17 @@
+import { useNavigate } from 'react-router-dom'
 import './ArtistAlbumsList.scss'
 
 export default function ArtistAlbumsList() {
+
+    const navigate = useNavigate();
+
+    function goToAlbum() {
+        navigate('/artist/album');
+    }
+
     return (
         <div id='artist-albums-wrapper'>
-            <div className='artist-album-bubble'>
+            <div className='artist-album-bubble' onClick={() => goToAlbum()}>
                 <h3>Album name</h3>
                 <hr className='line' />
                 <p>Number of songs: w | Total: xh, ym, zs </p>
