8bc59fd30a4a2c0927b1a0d490984248b26dc4b4
Franci <francesca1999@yahoo.com>
2024-07-16 18:53:37 +0300

    add navigate function in AlbumsList component

--- src/components/albums_list/AlbumsList.jsx
+++ src/components/albums_list/AlbumsList.jsx
@@ -1,6 +1,14 @@
+import { useNavigate } from 'react-router-dom'
 import './AlbumsList.scss'
 
 export default function AlbumsList() {
+
+    const navigate = useNavigate();
+
+    function goToAlbum() {
+        navigate('/artist/album')
+    }
+
     return (
         <div id="albums-section">
             <div className='artist-album-bubble' onClick={() => goToAlbum()}>
