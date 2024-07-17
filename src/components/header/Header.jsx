6e46061679df0f0c512692c4530138ffcefca81c
Franci <francesca1999@yahoo.com>
2024-07-17 12:00:32 +0300

    implement get suggestions request

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,26 @@
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useSearchParams } from 'react-router-dom'
 import './Header.scss'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function Header() {
 
     const navigate = useNavigate();
+
+    const [searchInput, setSearchInput] = useState("");
+    const [suggestions, setSuggestions] = useState([]);
+
+    useEffect(() => {
+        const getSuggestions = setTimeout(() => {
+            axios
+            .get(`http://127.0.0.1:5000/suggestions/${searchInput}`)
+            .then((response) => {
+                console.log(response.data);
+                setSuggestions(response.data);
+            });
+        }, 2000)
+        return () => clearTimeout(getSuggestions)
+    }, [searchInput]) 
 
     function goToHomepage() {
         navigate('/');
@@ -20,8 +37,8 @@ export default function Header() {
     return (
         <div className='header'>
             <div id='home-section' onClick={() => goToHomepage()}>Home</div>
-            <input type='text' id='search-box' placeholder='Search for a specific artist or album...' />
-
+            <input type='text' id='search-box' onChange={(event) => setSearchInput(event.target.value)} placeholder='Search for a specific artist or album...' />
+            
             <div id='menu'>
                 <div className='menu-section' onClick={() => goToArtists()}>Artists</div>
                 <div className='menu-section' onClick={() => goToAlbums()}>Albums</div>
